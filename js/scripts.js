e4199e623b7de9e1de69c237ef0d72a53e3d5fe5
azarzoza <azarzoza@gmail.com>
2015-03-09 20:47:24 -0700

    version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,69 @@
-
 var cheney = '99004';
 var spokane = '99220';
+var anchorage = '99504';
 
+ $(document).ready(function() {
   $.simpleWeather({
-    
-    location: '99504', // change zip
+    zipcode: '99504',
+    woeid: '2354490', //anchorage
+    location: '',
     unit: 'f',
-    
-    // Get _weather_ object
     success: function(weather) {
-      
-      // Project !
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li class="sunrise">'+weather.sunrise+'</li>';
+  
+  if(weather.temp > 75) {
+        $('body').animate({backgroundColor: '#F7AC57'}, 1500);
+      } else {
+        $('body').animate({backgroundColor: '#0091c2'}, 1500);
+      }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      // See console for _weather_ object
-      console.log(weather);
+      $("#weather").html(html);
     },
-  
-    // if error
-    error: function(error) {  
-      $('body').html('<p>' + error + '</p>');
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
     }
-  
   });
+  
+  $.simpleWeather({
+    zipcode: '',
+    woeid: '727232', //amsterdam
+    location: '',
+    unit: 'f',
+    sunrise: '',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li class="sunrise">'+weather.sunrise+'</li>';
+  
+      $("#weather2").html(html);
+    },
+    error: function(error) {
+      $("#weather2").html('<p>'+error+'</p>');
+    }
+  });
+  
+  $.simpleWeather({
+    zipcode: '',
+    woeid: '2378872', 
+    location: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li class="sunrise">'+weather.sunrise+'</li>';
+  
+      $("#weather3").html(html);
+    },
+    error: function(error) {
+      $("#weather3").html('<p>'+error+'</p>');
+    }
+  });
+  
+});
 
 
