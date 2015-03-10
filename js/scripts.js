866f4822e956004334b327dc93248884b9c94e11
azarzoza <azarzoza@gmail.com>
2015-03-10 11:34:20 -0700

    version2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
+
+
 var cheney = '99004';
 var spokane = '99220';
 var anchorage = '99504';
@@ -5,15 +10,20 @@ var anchorage = '99504';
  $(document).ready(function() {
   $.simpleWeather({
     zipcode: '99504',
-    woeid: '2354490', //anchorage
+    woeid: '2354490', //anchorage  forecast[X].date
     location: '',
     unit: 'f',
     success: function(weather) {
       html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
+
+      html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
+      html += '<ul><li class="currently">'+weather.currently+'</li>';
       html += '<li class="sunrise">'+weather.sunrise+'</li>';
       html += '<li class="sunset">'+weather.sunset+'</li>';
+
+      
+      
   
   if(weather.temp > 75) {
         $('body').animate({backgroundColor: '#F7AC57'}, 1500);
@@ -36,7 +46,8 @@ var anchorage = '99504';
     sunrise: '',
     success: function(weather) {
       html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li class="sunrise">'+weather.sunrise+'</li>';
       html += '<li class="sunset">'+weather.sunset+'</li>';
@@ -55,7 +66,28 @@ var anchorage = '99504';
     unit: 'f',
     success: function(weather) {
       html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li class="sunrise">'+weather.sunrise+'</li>';
+      html += '<li class="sunset">'+weather.sunset+'</li>';
+  
+      $("#weather3").html(html);
+    },
+    error: function(error) {
+      $("#weather3").html('<p>'+error+'</p>');
+    }
+  });
+
+   $.simpleWeather({
+    zipcode: '',
+    woeid: '2378872', 
+    location: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li class="sunrise">'+weather.sunrise+'</li>';
       html += '<li class="sunset">'+weather.sunset+'</li>';
