ec7cba6f1584f92dec12c7a2c8f5b666332f3c0b
azarzoza <azarzoza@gmail.com>
2015-03-14 21:46:08 -0700

    icons?

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,12 @@
     woeid: '2354490', //anchorage  forecast[X].date
     location: '',
     unit: 'f',
+    code: '',
+
+
     success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+
       html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
 
       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
@@ -15,28 +20,22 @@
       html += '<li class="sunrise">'+weather.sunrise+'</li>';
       html += '<li class="sunset">'+weather.sunset+'</li>';
 
-      
-      
-  
-  if(weather.temp > 75) {
-        $('body').animate({backgroundColor: '#F7AC57'}, 1500);
-      } else {
-        $('body').animate({backgroundColor: '#0091c2'}, 1500);
-      }
+   
+    
 
+    
       $("#weather").html(html);
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-  
-  $.simpleWeather({
+
+   $.simpleWeather({
     zipcode: '',
-    woeid: '727232', //amsterdam
+    woeid: '2378872', 
     location: '',
     unit: 'f',
-    sunrise: '',
     success: function(weather) {
       html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
        html += '<h3>'+weather.city+', '+weather.region+'</h3>';
@@ -45,6 +44,33 @@
       html += '<li class="sunrise">'+weather.sunrise+'</li>';
       html += '<li class="sunset">'+weather.sunset+'</li>';
   
+      $("#weather3").html(html);
+    },
+    error: function(error) {
+      $("#weather3").html('<p>'+error+'</p>');
+    }
+  });
+  
+});
+
+
+$.simpleWeather({
+    zipcode: '',
+    woeid: '727232', //amsterdam
+    location: '',
+    unit: 'f',
+    sunrise: '',
+    code: '',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li class="sunrise">'+weather.sunrise+'</li>';
+      html += '<li class="sunset">'+weather.sunset+'</li>';
+   
+
+
       $("#weather2").html(html);
     },
     error: function(error) {
@@ -71,36 +97,11 @@
       $("#weather3").html('<p>'+error+'</p>');
     }
   });
-
-   $.simpleWeather({
-    zipcode: '',
-    woeid: '2378872', 
-    location: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-       html += '<h3>'+weather.city+', '+weather.region+'</h3>';
-      html += '<h3 class="date">'+weather.forecast[0].date+'</h3>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li class="sunrise">'+weather.sunrise+'</li>';
-      html += '<li class="sunset">'+weather.sunset+'</li>';
-  
-      $("#weather3").html(html);
-    },
-    error: function(error) {
-      $("#weather3").html('<p>'+error+'</p>');
-    }
-  });
-  
-});
 // End of my cities with simpleWeather
 
 
 
 // Enter Zip get weather Button 
-// REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
-
 // On click button, get zip, then run Simple Weather
 $('button').on('click', function() {
   
@@ -119,13 +120,23 @@ $('button').on('click', function() {
       // Get & store city
       var city = weather.city;
 
+// Get and store current conditions
+
       var currently = weather.currently;
       
+var sunrise = weather.sunrise;
+var sunset = weather.sunset;
+var code = weather.code;
+
+
       // Output to hooks in HTML
-      $('.temp').text(temp);
+      $('.temp').text(temp).append('&deg;F');
       $('.city').text(city);
       $('.currently').text(currently);
-
+      $('.sunrise').text(sunrise).prepend('Sunrise ');
+      $('.sunset').text(sunset).prepend('Sunset ');
+      $('.code').text(code).prepend('#data-icon ');
+     
       // See console for all properties of object
       console.log(weather);
     },
@@ -176,6 +187,4 @@ $('button').on('click', function() {
   
   });
 
-
-
-
+  
